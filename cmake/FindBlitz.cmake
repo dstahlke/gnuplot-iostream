a6260d89561c9c2b004a063c8d6950873befd5a8
Jens K. Mueller <jkm@gluey.org>
2010-06-05 13:39:04 +0200

    Fix problem using Blitz
    FindPackageHandleStandardArgs sets <UPPERCASED_NAME>_FOUND but we used
    <NAME>_FOUND which wasn't set. To avoid this confusion all variables are now
    upper cased.

--- cmake/FindBlitz.cmake
+++ cmake/FindBlitz.cmake
@@ -6,24 +6,14 @@
 # - Find Blitz library
 # Find the native Blitz includes and library
 # This module defines
-#  Blitz_INCLUDE_DIR, where to find tiff.h, etc.
-#  Blitz_LIBRARIES, libraries to link against to use Blitz.
-#  Blitz_FOUND, If false, do not try to use Blitz.
-# also defined, but not for general use are
-#  Blitz_LIBRARY, where to find the Blitz library.
+#  BLITZ_INCLUDE_DIR, where to find tiff.h, etc.
+#  BLITZ_LIBRARY, libraries to link against to use Blitz.
+#  BLITZ_FOUND, If false, do not try to use Blitz.
 
-FIND_PATH(Blitz_INCLUDE_DIR blitz/blitz.h)
+FIND_PATH(BLITZ_INCLUDE_DIR blitz/blitz.h)
+FIND_LIBRARY(BLITZ_LIBRARY NAMES blitz)
 
-FIND_LIBRARY(Blitz_LIBRARY
-  NAMES blitz )
-
-# handle the QUIETLY and REQUIRED arguments and set Blitz_FOUND to TRUE if 
+# handle the QUIETLY and REQUIRED arguments and set BLITZ_FOUND to TRUE if 
 # all listed variables are TRUE
 INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(Blitz  DEFAULT_MSG  Blitz_LIBRARY  Blitz_INCLUDE_DIR)
-
-IF(Blitz_FOUND)
-  SET( Blitz_LIBRARIES ${Blitz_LIBRARY} )
-ENDIF(Blitz_FOUND)
-
-MARK_AS_ADVANCED(Blitz_INCLUDE_DIR Blitz_LIBRARY)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(Blitz DEFAULT_MSG BLITZ_LIBRARY BLITZ_INCLUDE_DIR)
