3f50a205f2d74f3f5112427cb74bcee227a33dbd
Dan Stahlke <dan@stahlke.org>
2013-01-13 16:34:55 -0500

    beginning to figure stuff out

--- tests.cc
+++ tests.cc
@@ -24,49 +24,14 @@ THE SOFTWARE.
 #include <vector>
 #include <math.h>
 
+#include <boost/utility.hpp>
 #include <boost/mpl/equal_to.hpp>
 #include <boost/mpl/vector.hpp>
 #include <boost/mpl/contains.hpp>
 #include <boost/mpl/and.hpp>
-#include "meta_util.h"
 
 #include "gnuplot-iostream.h"
 
-// from FireBreath
-//    typedef boost::mpl::vector
-//    <
-//        std::string, 
-//        std::wstring
-//    > pseudo_container_types;
-//
-//    template<typename T>
-//    struct plain_type {
-//        typedef typename boost::remove_const<typename boost::remove_reference<T>::type>::type type;
-//    };
-//
-//    template<class T>
-//    struct is_pseudo_container 
-//      : boost::mpl::contains<pseudo_container_types, typename plain_type<T>::type>::type
-//    {};
-//
-//    template<bool isClass, class T>
-//    struct is_container_helper
-//      : boost::mpl::and_< 
-//            boost::mpl::not_< is_pseudo_container<T> >,
-//            typename is_container_impl<T>::type >::type
-//    {};
-//
-//    template<class T>
-//    struct is_container_helper<false, T> 
-//      : boost::mpl::false_
-//    {};
-//
-//    template<class T>
-//    struct is_container
-//      : is_container_helper<boost::is_class<T>::value, T>
-//    {};
-
-// FIXME - experimental stuff
 template <class T>
 typename boost::enable_if_c<GnuplotEntry<T>::is_tuple, void>::type
 foo(const T &) {
@@ -91,54 +56,8 @@ foo(const T &) {
 	std::cout << "vec vec scalar" << std::endl;
 }
 
-template<class T>
-void f(T&, ...) {
-    std::cout << "flat" << std::endl;
-}
-
-template<class Cont>
-void f(Cont& c, typename Cont::iterator begin = Cont().begin(),
-                typename Cont::iterator end   = Cont().end()) {
-    std::cout << "container" << std::endl;
-}
-
-template <class T>
-typename boost::enable_if<boost::mpl::not_<FB::meta::is_container<T> >, void>
-g(const T &) {
-	std::cout << "g flat" << std::endl;
-}
-
-template <class T>
-typename boost::enable_if<FB::meta::is_container<T>, void>
-g(const T &) {
-	std::cout << "g cont" << std::endl;
-}
-
-template <class T>
-void h(const T &) {
-	std::cout << "is_container: " << FB::meta::is_container<T>::value << std::endl;
-}
-
-template <class T, bool C>
-class B { };
-
-template <class T>
-class B<T, false> {
-public: void go(const T &) { std::cout << "B flat" << std::endl; }
-};
-
-template <class T>
-class B<T, true> {
-public: void go(const T &) { std::cout << "B cont" << std::endl; }
-};
-
-template <class T>
-void a(const T &x) {
-	B<T, FB::meta::is_container<T>::value>().go(x);
-}
-
 template <typename T>
-class has_helloworld {
+class is_container {
     typedef char one;
     typedef long two;
 
@@ -149,38 +68,148 @@ public:
     enum { value = sizeof(test<T>(NULL, NULL)) == sizeof(char) };
 };
 
+//template <class T>
+//magic_iter<typename T::iterator>
+//make_magic_iter(T &container) {
+//	return boost::iterator_range<typename T::iterator>(container.begin(), container.end());
+//}
+
+template <class T>
+typename boost::enable_if<is_container<T> >::type
+a(const T &) {
+	std::cout << "a:cont" << std::endl;
+}
+
+template <class T>
+typename boost::enable_if_c<!is_container<T>::value>::type
+a(const T &) {
+	std::cout << "a:flat" << std::endl;
+}
+
+template <class T, class Enable=void>
+class RangeVecBase : public std::vector<std::pair<T, T> > {
+public:
+	void inc() {
+		for(size_t i=0; i<this->size(); i++) {
+			++(this->at(i).first);
+		}
+	}
+
+	bool is_done() {
+		return this->empty() || (this->at(0).first == this->at(0).second);
+	}
+};
+
+template <class T, class Enable=void>
+class RangeVec : public RangeVecBase<T> { };
+
+template <class T>
+class RangeVec<T, typename boost::enable_if<is_container<typename T::value_type> >::type> : public RangeVecBase<T> {
+public:
+	RangeVec<typename T::value_type::const_iterator> deref() {
+		RangeVec<typename T::value_type::const_iterator> ret;
+		for(size_t i=0; i<this->size(); i++) {
+			ret.push_back(std::make_pair(
+					this->at(i).first->begin(), this->at(i).first->end()));
+		}
+		return ret;
+	}
+};
+
+template <class T>
+RangeVec<typename T::value_type::const_iterator>
+get_range_vec(const T &arg) {
+	RangeVec<typename T::value_type::const_iterator> ret;
+	// FIXME - why doesn't cbegin/cend compile?
+	for(typename T::const_iterator outer_it=arg.begin(); outer_it != arg.end(); ++outer_it) {
+		ret.push_back(std::make_pair(outer_it->begin(), outer_it->end()));
+	}
+	return ret;
+}
+
+template <class T>
+void send_CL_rvec(RangeVec<T> arg) {
+	if(arg.empty()) return;
+	while(!arg.is_done()) {
+		for(size_t i=0; i<arg.size(); i++) {
+			if(i) std::cout << " ";
+			GnuplotEntry<typename T::value_type>::send(std::cout, *arg[i].first);
+		}
+		arg.inc();
+		std::cout << "\n";
+	}
+}
+
+template <class T>
+void send_CL(const T &arg) {
+	send_CL_rvec(get_range_vec(arg));
+}
+
+template <class T>
+void send_CBL_rvec(RangeVec<T> arg) {
+	if(arg.empty()) return;
+	while(!arg.is_done()) {
+		RangeVec<typename T::value_type::const_iterator> inner = arg.deref();
+		send_CL_rvec(inner);
+		arg.inc();
+		std::cout << "\n";
+	}
+}
+
+template <class T>
+void send_CBL(const T &arg) {
+	send_CBL_rvec(get_range_vec(arg));
+}
+
 int main() {
 	//Gnuplot gp("cat");
 
 	const int NX=3, NY=4;
-	std::vector<std::vector<double> > scalar_array(NX);
-	std::vector<std::vector<std::pair<double, double> > > tuple_array(NX);
 
-	for(int y=0; y<NY; y++) {
-		for(int x=0; x<NX; x++) {
-			scalar_array[x].push_back(x*10+y);
-			tuple_array[x].push_back(std::make_pair(100+x*10+y, 200+x*10+y));
+	double s = 0.0;
+	std::pair<double, double> t;
+	std::vector<double> vs;
+	std::vector<std::pair<double, double> > vt;
+	std::vector<std::vector<double> > cvs(2);
+	std::vector<std::vector<double> > vvs(NX);
+	std::vector<std::vector<std::pair<double, double> > > vvt(NX);
+	std::vector<std::vector<std::vector<double> > > cvvs(2);
+
+	cvvs[0].resize(NX);
+	cvvs[1].resize(NX);
+
+	for(int x=0; x<NX; x++) {
+		vs.push_back(x);
+		vt.push_back(std::make_pair(100+x, 200+x));
+		cvs[0].push_back(100+x);
+		cvs[1].push_back(200+x);
+		for(int y=0; y<NY; y++) {
+			vvs[x].push_back(x*10+y);
+			vvt[x].push_back(std::make_pair(100+x*10+y, 200+x*10+y));
+			cvvs[0][x].push_back(100+x*10+y);
+			cvvs[1][x].push_back(200+x*10+y);
 		}
 	}
 
 	//gp.send(scalar_array);
 	//gp.send(tuple_array);
 
-	double s = 0.0;
-	std::pair<double, double> t;
-	std::vector<double> vs;
-	std::vector<std::pair<double, double> > vt;
 
-	foo(scalar_array);
-	foo(tuple_array);
+	foo(vvs);
+	foo(vvt);
 	//foo(0.0);
-	foo(std::make_pair(0.0, 1.0));
+	foo(t);
 	//foo(std::vector<double>());
-	foo(std::vector<std::vector<bool> >());
 
 	a(s);
 	a(vs);
 
-	std::cout << "hb=" << has_helloworld<std::pair<double, double> >::value << std::endl;
-	std::cout << "hb=" << has_helloworld<std::vector<double> >::value << std::endl;
+	std::cout << "hb=" << is_container<double>::value << std::endl;
+	std::cout << "hb=" << is_container<std::pair<double, double> >::value << std::endl;
+	std::cout << "hb=" << is_container<std::vector<double> >::value << std::endl;
+
+	send_CL(cvs);
+	std::cout << "e" << std::endl;
+	send_CBL(cvvs);
+	std::cout << "e" << std::endl;
 }
