cc8c5bf73cf4137134ddca8fbd6e4df2e6232254
Dan Stahlke <dan@stahlke.org>
2013-01-19 22:52:44 -0500

    more

--- tests_v3.cc
+++ tests_v3.cc
@@ -10,7 +10,7 @@
 #include <boost/array.hpp>
 
 template <typename T>
-class is_container {
+class is_like_stl_container {
     typedef char one;
     typedef long two;
 
@@ -21,71 +21,89 @@ public:
     enum { value = sizeof(test<T>(NULL, NULL)) == sizeof(char) };
 };
 
-template <class T>
-typename boost::enable_if<is_container<T> >::type
-a(const T &) {
-	std::cout << "a:cont" << std::endl;
-}
+// Error messages involving this stem from treating something that was not a container as if it
+// was.
+class WasNotContainer { };
 
-template <class T>
-typename boost::enable_if_c<!is_container<T>::value>::type
-a(const T &) {
-	std::cout << "a:flat" << std::endl;
-}
-
-template <typename T>
-class ArrayTraits;
-
-template <typename TI, typename TV, typename Enable=void>
-class RangeBase {
-public:
-	RangeBase() { }
-	RangeBase(const TI &_it, const TI &_end) : it(_it), end(_end) { }
-
-	typedef TV value_type;
-	typedef typename ArrayTraits<TV>::range_type subiter_type;
-	enum { is_container = ArrayTraits<TV>::is_container };
-
-	bool is_end() { return it == end; }
-
-	void inc() { ++it; }
-
-	value_type deref() const {
-		return *it;
-	}
-
-	subiter_type deref_subiter() const {
-		return ArrayTraits<TV>::get_range(*it);
-	}
-
-private:
-	TI it, end;
-};
-
-template <typename TI, typename TV>
-class Range : public RangeBase<TI, TV> {
-public:
-	Range() { }
-	Range(const TI &_it, const TI &_end) : RangeBase<TI, TV>(_it, _end) { }
-};
-
-template <typename T>
+template <typename T, typename Enable=void>
 class ArrayTraits {
 public:
-	typedef void range_type;
+	typedef WasNotContainer range_type;
 	enum { is_container = false };
 
-	static void get_range(const T &) { }
+	static range_type get_range(const T &) { }
 };
 
 template <typename T>
-class ArrayTraits<std::vector<T> > {
+class ArrayTraits<T, typename boost::enable_if<is_like_stl_container<T> >::type> {
+	template <typename TI, typename TV, typename Enable=void>
+	class STLIteratorRange {
+	public:
+		STLIteratorRange() { }
+		STLIteratorRange(const TI &_it, const TI &_end) : it(_it), end(_end) { }
+
+		typedef TV value_type;
+		typedef typename ArrayTraits<TV>::range_type subiter_type;
+		enum { is_container = ArrayTraits<TV>::is_container };
+
+		bool is_end() { return it == end; }
+
+		void inc() { ++it; }
+
+		value_type deref() const {
+			return *it;
+		}
+
+		subiter_type deref_subiter() const {
+			return ArrayTraits<TV>::get_range(*it);
+		}
+
+	private:
+		TI it, end;
+	};
+
 public:
-	typedef Range<typename std::vector<T>::const_iterator, T> range_type;
+	typedef STLIteratorRange<typename T::const_iterator, typename T::value_type> range_type;
 	enum { is_container = true };
 
-	static range_type get_range(const std::vector<T> &arg) {
+	static range_type get_range(const T &arg) {
 		return range_type(arg.begin(), arg.end());
+	}
+};
+
+template <typename T, size_t N>
+class ArrayTraits<T[N]> {
+	class CArrayRange {
+	public:
+		CArrayRange() : p(NULL), it(0) { }
+		CArrayRange(const T *_p) : p(_p), it(0) { }
+
+		typedef T value_type;
+		typedef typename ArrayTraits<T>::range_type subiter_type;
+		enum { is_container = ArrayTraits<T>::is_container };
+
+		bool is_end() { return it == N; }
+
+		void inc() { ++it; }
+
+		value_type deref() const {
+			return p[it];
+		}
+
+		subiter_type deref_subiter() const {
+			return ArrayTraits<T>::get_range(p[it]);
+		}
+
+	private:
+		const T *p;
+		size_t it;
+	};
+public:
+	typedef CArrayRange range_type;
+	enum { is_container = true };
+
+	static range_type get_range(const T (&arg)[N]) {
+		return range_type(arg);
 	}
 };
 
@@ -210,10 +228,14 @@ int main() {
 	std::vector<std::vector<double> > vvd(NX);
 	std::vector<std::vector<int> > vvi(NX);
 	std::vector<std::vector<std::vector<int> > > vvvi(NX);
+	int ai[NX];
+	boost::array<int, NX> bi;
 
 	for(int x=0; x<NX; x++) {
 		vd.push_back(x+7.5);
 		vi.push_back(x+7);
+		ai[x] = x+7;
+		bi[x] = x+70;
 		for(int y=0; y<NY; y++) {
 			vvd[x].push_back(100+x*10+y);
 			vvi[x].push_back(200+x*10+y);
@@ -224,7 +246,9 @@ int main() {
 		}
 	}
 
-	plot(std::make_pair(vd, vi));
+	plot(std::make_pair(vd, std::make_pair(vi, bi)));
 	plot(std::make_pair(vvd, vvi));
+	plot(ai);
+	//plot(std::make_pair(ai, bi));
 	//plot(std::make_pair(vvd, std::make_pair(vvi, vvvi)));
 }
