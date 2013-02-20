f91de2616128110343a43e74369b381c2b8cc2aa
Dan Stahlke <dan@stahlke.org>
2013-02-19 22:45:07 -0500

    misc

---
+++ vc-path.bat
@@ -0,0 +1,3 @@
+rem This is just here for my own tests.  To be removed eventually.
+set PATH=C:\Program Files (x86)\Microsoft Visual C++ Compiler Nov 2012 CTP\bin;%PATH%
+set INCLUDE=C:\Program Files (x86)\Microsoft Visual C++ Compiler Nov 2012 CTP\include;%INCLUDE%
