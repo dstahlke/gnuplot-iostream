a3167d7f89887fb0b5f33d21d2ce0655fa102264
Dan Stahlke <dan@stahlke.org>
2013-02-24 21:55:11 -0500

    trying to understand arrays vs. templates

--- tests_v4.cc
+++ tests_v4.cc
@@ -7,81 +7,25 @@
 #include <boost/array.hpp>
 #include <boost/tuple/tuple.hpp>
 
-// for debugging
-#include <typeinfo>
-#include <cxxabi.h>
-
-template <typename T>
-std::string get_typename() {
-	int status;
-	char *name;
-	name = abi::__cxa_demangle(typeid(T).name(), 0, 0, &status);
-	assert(!status);
-	return std::string(name);
-}
-
-template <typename T>
-std::string get_typename_of(const T &) {
-	return get_typename<T>();
-}
+// my debug routines:
+#include <gcc-features.h>
 
 #include "gnuplot-iostream.h"
 
 using namespace gnuplotio;
 
-//template <typename T>
-//void runtest(std::string header, const T &arg) {
-//	Gnuplot gp("cat");
-//	std::cout << "--- " << header << " -------------------------------------" << std::endl;
-//	std::cout << "ncols=" << gnuplotio::ArrayTraits<T>::ncols << std::endl;
-//	std::cout << "depth=" << gnuplotio::ArrayTraits<T>::depth << std::endl;
-//	std::cout << "val=" << get_typename<typename gnuplotio::ArrayTraits<T>::value_type>() << std::endl;
-//	std::cout << "bintype=[" << gp.binfmt(arg) << "]" << std::endl;
-//	std::cout << "range_type=" << get_typename<typename gnuplotio::ArrayTraits<T>::range_type>() << std::endl;
-//	gp.send(arg);
-//	gp.file(arg, "unittest-output/"+header+".txt");
-//	gp.binaryFile(arg, "unittest-output/"+header+".bin");
-//}
-
 template <typename T>
-void go(const T &arg) {
-	typedef typename boost::remove_reference<typename boost::tuples::element<0, T>::type>::type U;
+void go(const T &) {
+	typedef typename ArrayTraits<int (&) [10]>::range_type U;
+	std::cout << get_typename<T>() << std::endl;
 	std::cout << get_typename<U>() << std::endl;
-	std::cout << get_typename<typename U::value_type>() << std::endl;
-	std::cout << (is_like_stl_container<U>::value) << std::endl;
-	std::cout << get_typename<typename is_like_stl_container<U>::type>() << std::endl;
-	std::cout << get_typename<typename gnuplotio::ArrayTraits<T>::HT>() << std::endl;
-	std::cout << get_typename<typename gnuplotio::ArrayTraits<T>::TT>() << std::endl;
-	std::cout << get_typename<typename gnuplotio::ArrayTraits<T>::parent>() << std::endl;
-	std::cout << get_typename<typename gnuplotio::ArrayTraits<T>::value_type>() << std::endl;
-	auto range = gnuplotio::ArrayTraits<T>::get_range(arg);
-	std::cout << get_typename_of(range) << std::endl;
-	auto val = range.deref();
-	gnuplotio::TextSender<decltype(val)>::send(std::cout, val);
-	std::cout << std::endl;
 }
 
 int main() {
-	gnuplotio::set_debug_array_print(true);
+	//int x[10], y[10];
+	//auto t = std::tie(x, y);
+	//go(t);
 
-	const int NX=3;
-	std::vector<double> vd;
-	std::vector<int> vi;
-	std::vector<float> vf;
-
-	for(int x=0; x<NX; x++) {
-		vd.push_back(x+7.1);
-		vi.push_back(x+7.2);
-		vf.push_back(x+7.3);
-	}
-
-	//go(boost::make_tuple(vd, vi));
-	//runtest("vd,vi,vf", boost::make_tuple(vd, vi));
-
-	//std::tuple<double, int, int> tup(1,2,3);
-	//gnuplotio::send_entry(std::cout, tup);
-	//std::cout << std::endl;
-
-	//gnuplotio::generic_sender_level0(std::cout, boost::tie(vd, vi), gnuplotio::Mode1D(), gnuplotio::ModeText());
-	go(boost::tie(vd, vi));
+	typedef typename ArrayTraits<int (&) [10]>::range_type U;
+	std::cout << get_typename<U>() << std::endl;
 }
